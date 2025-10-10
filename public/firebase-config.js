db5001b2647b839231d8ceef69e86f540984f6cb
Kiyoshiakira <shaunessy24@gmail.com>
2025-10-10 11:01:07 -0500

    Format firebase-config.js for consistency

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -3,13 +3,13 @@
 // Find these values in: Firebase Console > Project Settings > General > Your apps > SDK setup and configuration
 
 const firebaseConfig = {
-      apiKey: "AIzaSyD4dVzjsGLI1znvU2-1zhZfLKFZLf8PXWU",
-      authDomain: "ideaforge-e92ff.firebaseapp.com",
-      projectId: "ideaforge-e92ff",
-      storageBucket: "ideaforge-e92ff.appspot.com",
-      messagingSenderId: "700329776913",
-      appId: "1:700329776913:web:d912716f7df8ef698c9eb0",
-      measurementId: "G-2YX4CRP00G"
+  apiKey: "AIzaSyD4dVzjsGLI1znvU2-1zhZfLKFZLf8PXWU",
+  authDomain: "ideaforge-e92ff.firebaseapp.com",
+  projectId: "ideaforge-e92ff",
+  storageBucket: "ideaforge-e92ff.appspot.com",
+  messagingSenderId: "700329776913",
+  appId: "1:700329776913:web:d912716f7df8ef698c9eb0",
+  measurementId: "G-2YX4CRP00G"
 };
 
 // Initialize Firebase
